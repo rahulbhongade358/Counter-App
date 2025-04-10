56471d45f2470039dc61909386b3001bfaad79ae
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-04-10 16:39:00 +0530

    price card added

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import './App.css'
 function App() {
   const [Quantity1, setQuantity1] = useState(0)
+  const [Quantity2, setQuantity2] = useState(0)
 
   let Food = "Burger"
   const price = 40;
-
+  let finaltotal1 = Quantity1 * price
+  let finaltotal2=Quantity2*price
+   let totalAmount = finaltotal1+finaltotal2
   return (
     <div className='component'>
       <div className='main-card'>
@@ -32,6 +35,42 @@ function App() {
             >+</button>
           </div>
         </div>
+
+      </div>
+      <div className='main-card'>
+        <h1>Foodies</h1>
+        <div className='food-card'>
+          <h2>{Food}</h2>
+          <img src="https://www.foodandwine.com/thmb/XE8ubzwObCIgMw7qJ9CsqUZocNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/MSG-Smash-Burger-FT-RECIPE0124-d9682401f3554ef683e24311abdf342b.jpg"alt="" />
+          <p>Price : ₹{price}</p> 
+          <div className='quantity-card'>
+            <button 
+            onClick={()=>{
+              if (Quantity2>0) {
+                setQuantity1(Quantity2-1)
+              }
+            }}
+             >-</button>
+            <p>{Quantity2}</p>
+            <button 
+            onClick={()=> {
+              if (Quantity2<15)
+                 {
+               setQuantity2(Quantity2+1)
+            }}}
+            >+</button>
+          </div>
+        </div>
+
+      </div>
+      <div className='price-card'>
+      <h2>Bill Summary</h2>
+      <p>
+          Food: {Food} <br />
+          Quantity:-{Quantity1} : ₹ {finaltotal1} <br />
+          Quantity:-{Quantity2} : ₹ {finaltotal2} <br />
+          Total Amount : ₹ {totalAmount}
+        </p>
 
       </div>
     </div>
